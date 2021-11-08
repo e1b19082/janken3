7d9a47adc2b0f24167eac4b07094171613f19bc9
e1b19082 <itpassport.kh@gmail.com>
2021-11-08 18:13:40 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z0209/kaizi/janken3/Janken3Application.java
@@ -0,0 +1,13 @@
+package oit.is.z0209.kaizi.janken3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken3Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z0209/kaizi/janken3/Janken3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0209.kaizi.janken3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
